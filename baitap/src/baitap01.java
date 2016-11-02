274dae4bfe684c055970195dc94dbb3ffa994cae
PhuongTrang <phuongtrang1397@gmail.com>
2016-11-02 17:17:58 +0700

    add all

---
+++ baitap/src/baitap01.java
@@ -0,0 +1,21 @@
+import javax.swing.JFrame;
+
+
+public class baitap01 extends JFrame {
+public baitap01(){
+	setSize(400,300);
+	setLayout(null);
+	setTitle("BT");
+	
+}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		baitap01 wMain = new baitap01();
+		wMain.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		wMain.setVisible(true);
+	}
+
+}
