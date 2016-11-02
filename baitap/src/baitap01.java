62379e08010c2573ef60ca793ea222a2fca8b644
PhuongTrang <phuongtrang1397@gmail.com>
2016-11-02 17:43:08 +0700

    add 20 button

--- baitap/src/baitap01.java
+++ baitap/src/baitap01.java
@@ -1,12 +1,23 @@
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 
 public class baitap01 extends JFrame {
+	
 public baitap01(){
 	setSize(400,300);
 	setLayout(null);
 	setTitle("BT");
 	
+	
+	
+	setLayout(new FlowLayout());
+	for(int i =0; i<=20; i++){
+		add(new JButton(String.valueOf(i)));
+		
+	}
 }
 	/**
 	 * @param args
